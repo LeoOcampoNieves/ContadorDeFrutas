6417b2a58915bf1696b46edc4f0ea484ea106f83
Leonis Ocampo Nieves <leonis.ocampo@outlook.com>
2020-04-14 09:14:50 -0500

    Terminando el proyecto

--- src/store/index.js
+++ src/store/index.js
@@ -4,12 +4,30 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+	state: {
+		frutas: [
+			{nombre: 'Manzana', cantidad: 0},
+			{nombre: 'Pera', cantidad: 0},
+			{nombre: 'Naranja', cantidad: 0},
+			{nombre: 'mora', cantidad: 0}
+		]
+  	},
+	  
+	mutations: {
+		aumentar(state, index) {
+			state.frutas[index].cantidad ++
+		},
+
+		reiniciar(state) {
+			state.frutas.forEach(elemento => {
+				elemento.cantidad = 0
+			})
+		}
+  	},
+	  
+	actions: {
+  	},
+  
+	modules: {
+  	}
 })
